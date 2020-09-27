525a0446e807b90192efdc734120f02f09a18394
Luciano Santos <luciano_z7@hotmail.com>
2020-09-27 20:34:17 -0300

    [Feature] Adding travis file

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,4 @@
 from flask_pymongo import PyMongo, MongoClient
 
-client = MongoClient('mongodb://service-mongodb:27017/', username='admin', password='password')
+client = MongoClient('mongodb://admin-mongodb:27017/', username='admin', password='password')
 db = client.smartDevApiService
