7faf7478a4fe5a090c56425fbfaa5636cf8abdaf
Luciano Santos <luciano_z7@hotmail.com>
2020-10-12 20:59:29 -0300

    [Feature] Adding procfile

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,12 @@
 from flask_pymongo import MongoClient
+import os
+
+password = os.environ["MONGOPASSWORD"]
+dbname = os.environ["DBNAME"]
+env = os.environ["ENVIRONMENT"]
 
 client = MongoClient(
-    'mongodb://contract-mongodb:27017/',
-    username='admin',
-    password='password'
+    "mongodb+srv://smartAdmin:"
+    + password + "@smartvit.6s6r9.mongodb.net/"
+    + dbname + "?retryWrites=true&w=majority"
 )
-db = client.smartDevApiService
